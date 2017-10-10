de6a9ca420203b6d4a583c9ba14a3197081484cc
TX <thawsoar@gmail.com>
2017-10-10 16:59:17 +0800

    Site updated: 2017-10-10 16:59:15

---
+++ js/src/titlechange.js
@@ -0,0 +1,16 @@
+ var OriginTitile = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/TEP.ico");
+         document.title = '╭(°A°`)╮ 页面炸啦 ~ | 快来看呀！';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/favicon.ico");
+         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitile;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitile;
+         }, 2000);
+     }
+ });
