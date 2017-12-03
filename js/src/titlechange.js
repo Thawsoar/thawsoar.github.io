6f1519a91268e0b2319e932d72a1d949a29c9e65
TX <thawsoar@gmail.com>
2017-12-03 22:47:10 +0800

    Site updated: 2017-12-03 22:47:08

--- js/src/titlechange.js
+++ js/src/titlechange.js
@@ -1,16 +1,15 @@
  var OriginTitile = document.title;
  var titleTime;
- document.addEventListener('visibilitychange', function () {
-     if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/TEP.ico");
-         document.title = '╭(°A°`)╮ 页面炸啦 ~ | 快来看呀！';
-         clearTimeout(titleTime);
-     }
-     else {
-         $('[rel="icon"]').attr('href', "/favicon.ico");
-         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitile;
-         titleTime = setTimeout(function () {
-             document.title = OriginTitile;
-         }, 2000);
-     }
+ document.addEventListener('visibilitychange', function() {
+   if (document.hidden) {
+     $('[rel="icon"]').attr('href', "/img/TEP.ico");
+     document.title = '╭(°A°`)╮ 你快回来 ~ | ';
+     clearTimeout(titleTime);
+   } else {
+     $('[rel="icon"]').attr('href', "/favicon.ico");
+     document.title = '(ฅ>ω<*ฅ) 我一人承受不来！~' + OriginTitile;
+     titleTime = setTimeout(function() {
+       document.title = OriginTitile;
+     }, 2000);
+   }
  });
